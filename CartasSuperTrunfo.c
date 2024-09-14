3e6df8ce7506f2b91c3a7b577e6d7a26c528ad69
Alejandro-Reis <alejandroreis00@gmail.com>
2024-09-14 02:33:02 +0000

    Cartas Supoer Trunfo. Cadastrar cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,21 +1,105 @@
 #include <stdio.h>
+int main(){
+    char estado1;
+    printf("Escolha o primeiro estado de A a H: ");
+    scanf("%c",&estado1);
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
+    int populacao1;
+    printf("Digite a quantidade de populacao desse estado: ");
+    scanf("%d",&populacao1);
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    int area1;
+    printf("Digite a quantidade de area desse estado: ");
+    scanf("%d",&area1);
 
-    return 0;
+    int pib1;
+    printf("Digite o Pib desse estado: ");
+    scanf("%d",&pib1);
+
+    int pontostur1;
+    printf("Digite o numero de pontos turisticos desse estado: ");
+    scanf("%d", &pontostur1);
+
+
+
+  char estado2;
+    printf("\nEscolha o segundo estado de A a H: ");
+    scanf(" %c", &estado2);
+
+    int populacao2;
+    printf("Digite a quantidade de populacao desse estado: ");
+    scanf("%d",&populacao2);
+
+    int area2;
+    printf("Digite a quantidade de area desse estado: ");
+    scanf("%d",&area2);
+
+    int pib2;
+    printf("Digite o Pib desse estado: ");
+    scanf("%d",&pib2);
+
+    int pontostur2;
+    printf("Digite o numero de pontos turisticos desse estado: ");
+    scanf("%d",&pontostur2);
+
+
+
+  char estado3;
+    printf("\nEscolha o terceiro estado de A a H: ");
+    scanf(" %c", &estado3);
+
+    int populacao3;
+    printf("Digite a quantidade de populacao desse estado: ");
+    scanf("%d",&populacao3);
+
+    int area3;
+    printf("Digite a quantidade de area desse estado: ");
+    scanf("%d",&area3);
+
+    int pib3;
+    printf("Digite o Pib desse estado: ");
+    scanf("%d",&pib3);
+
+    int pontostur3;
+    printf("Digite o numero de pontos turisticos desse estado: ");
+    scanf("%d",&pontostur3);
+
+
+  char estado4;
+    printf("\nEscolha o quarto estado de A a H: ");
+    scanf(" %c", &estado4);
+
+    int populacao4;
+    printf("Digite a quantidade de populacao desse estado: ");
+    scanf("%d",&populacao4);
+
+    int area4;
+    printf("Digite a quantidade de area desse estado: ");
+    scanf("%d",&area4);
+
+    int pib4;
+    printf("Digite o Pib desse estado: ");
+    scanf("%d",&pib4);
+
+    int pontostur4;
+    printf("Digite o numero de pontos turisticos desse estado: ");
+    scanf("%d",&pontostur4);
+
+
+printf("\nEntão conforme suas escolhas... \n O status do seu estado: %c e cidade 01 foi: \n Populacao: %dmil\n Area: %dkm\n Pib: %dm\n Pontos turisticos:%d \n\n\n",estado1,pontostur1,area1,pib1,pontostur1);
+
+
+printf("Então conforme suas escolhas... \n O status do seu estado: %c e cidade 02 foi: \n Populacao: %dmil\n Area: %dkm\n Pib: %dm\n Pontos turisticos:%d \n\n\n",estado2,pontostur2,area2,pib2,pontostur2);
+
+ 
+printf("Então conforme suas escolhas... \n O status do seu estado: %c e cidade 03 foi: \n Populacao: %dmil\n Area: %dkm\n Pib: %dm\n Pontos turisticos:%d \n\n\n",estado3,pontostur3,area3,pib3,pontostur3);
+
+
+ printf("Então conforme suas escolhas... \n O status do seu estado: %c e cidade 04 foi: \n Populacao: %dmil\n Area: %dkm\n Pib: %dm\n Pontos turisticos:%d \n\n\n",estado4,pontostur4,area4,pib4,pontostur4);
+
+
+
+
+
+
 }
